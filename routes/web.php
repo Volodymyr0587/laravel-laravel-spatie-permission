22b95afe3349c2453b2a1086395de2c2c9d03326
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-06 18:02:33 +0200

    Create Admin controllers [IndexController, PermissionController, RoleController]; create views for roles and permissions

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,8 +31,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/admin', function () {
-    return view('admin.index');
-})->middleware(['auth', 'role:admin', 'verified'])->name('admin.index');
+Route::middleware(['auth', 'role:admin', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [IndexController::class,'index'])->name('index');
+    Route::resource('/roles', RoleController::class);
+    Route::resource('/permissions', PermissionController::class);
+});
 
 require __DIR__.'/auth.php';
