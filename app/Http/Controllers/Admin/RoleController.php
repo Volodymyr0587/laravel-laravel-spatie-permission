4fbb309891e3d511d3fab48468cef730ee388a4f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-03-07 16:48:37 +0200

    Assign Permission to Role

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
@@ -29,7 +30,8 @@ class RoleController extends Controller
 
     public function edit(Role $role)
     {
-        return view('admin.roles.edit', compact('role'));
+        $permissions = Permission::all();
+        return view('admin.roles.edit', compact('role', 'permissions'));
     }
 
     public function update(Request $request, Role $role)
@@ -44,5 +46,15 @@ class RoleController extends Controller
     {
         $role->delete();
         return to_route('admin.roles.index')->with('message','Role deleted successfully');
+    }
+
+    public function givePermission(Request $request, Role $role)
+    {
+        if ($role->hasPermissionTo($request->permission)) {
+            return back()->with('message', "Permission $request->permission exists.");
+        }
+
+        $role->givePermissionTo($request->permission);
+        return back()->with("message", "Permission added.");
     }
 }
--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -18,12 +18,46 @@
                                         class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                                 </div>
                                 @error('name')
-                                    <span class="text-red-400 text-sm">{{ $message }}</span>
+                                <span class="text-red-400 text-sm">{{ $message }}</span>
                                 @enderror
                             </div>
 
                             <div class="sm:col-span-6 pt-5">
-                                <button type="submit" class="px-4 py-2 text-white bg-green-500 hover:bg-green-700 rounded-md">Update</button>
+                                <button type="submit"
+                                    class="px-4 py-2 text-white bg-green-500 hover:bg-green-700 rounded-md">Update</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="mt-6 p-2">
+                    <h2 class="text-2xl font-semibold">Role Permissions</h2>
+                    <div class="mt-4 p-2">
+                        @if ($role->permissions)
+                        @foreach ($role->permissions as $role_permission)
+                        <span>{{ $role_permission->name }}</span>
+                        @endforeach
+                        @endif
+                    </div>
+                    <div class="max-w-xl">
+                        <form method="POST" action="{{ route('admin.roles.permissions', $role->id) }}">
+                            @csrf
+
+                            <div class="sm:col-span-6">
+                                <label for="permission" class="block text-sm font-medium text-gray-700">Permission</label>
+                                <select id="permission" name="permission" autocomplete="permission-name"
+                                    class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    @foreach ($permissions as $permission)
+                                        <option value="{{ $permission->name }}">{{ $permission->name }}</option>
+                                    @endforeach
+                                </select>
+                                @error('name')
+                                <span class="text-red-400 text-sm">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <div class="sm:col-span-6 pt-5">
+                                <button type="submit"
+                                    class="px-4 py-2 text-white bg-green-500 hover:bg-green-700 rounded-md">Assign</button>
                             </div>
                         </form>
                     </div>
